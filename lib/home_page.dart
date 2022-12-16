12db9ac8a3625d2f946d1f768c87cf4d87324b80
Aktai49 <solnc349@gmail.com>
2022-12-16 17:00:10 +0300

    Quiz app

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:test/model/quizMod.dart';
+import 'package:test/widgets/quizBtn.dart';
+import 'package:test/widgets/resultBtn.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,102 +11,62 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int index = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 13, 10, 51),
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 13, 10, 51),
-        title: const Text(
-          'Tapshyrma 7',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-      ),
+      backgroundColor: const Color.fromARGB(255, 13, 10, 51),
+      appBar: _appBar(),
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
         child: Column(
           children: [
-            SizedBox(
-              height: 150,
-            ),
+            const Spacer(),
             Text(
-              'Kg 7 obl barby?',
+              quizzes[index].questions,
               textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.white, fontSize: 30),
+              style: const TextStyle(color: Colors.white, fontSize: 30),
             ),
-            SizedBox(
+            const SizedBox(
               height: 100,
             ),
-            QuizButton(tuuraBtn: true,),
-            SizedBox(
+            const QuizButton(
+              tuuraBtnbu: true,
+            ),
+            const SizedBox(
               height: 20,
             ),
-            SizedBox(
-              height: 50,
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red,
-                ),
-                child: Text(
-                  'Kata',
-                  style: TextStyle(fontSize: 24),
-                ),
-              ),
+            const QuizButton(
+              tuuraBtnbu: false,
             ),
+            const SizedBox(height: 50),
             Row(
-              children: [
-                Icon(
-                  Icons.mail,
-                  color: Colors.green,
-                ),
-                Icon(
-                  Icons.close,
-                  color: Colors.red,
-                )
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                ResultIcon(tuurIconbu: true),
+                ResultIcon(tuurIconbu: false),
               ],
-            )
+            ),
+            const SizedBox(height: 50),
           ],
         ),
       ),
     );
   }
-}
 
-class QuizButton extends StatelessWidget {
-  const QuizButton({
-    Key? key,
-    required this.tuuraBtn,
-  }) : super(key: key);
-
-  final bool tuuraBtn;
-
-  @override
-  Widget build(BuildContext context) {
-    if (tuuraBtn == true) {
-      'tuura';}
-    else{
-      'kata';
-    }
-
-    return SizedBox(
-      width: double.infinity,
-      height: 50,
-      child: ElevatedButton(
-        onPressed: () {},
-        style: ElevatedButton.styleFrom(
-            backgroundColor: tuuraBtn ? Colors.green : Colors.red),
-        child: Text(
-          'Tuura',
-          style: TextStyle(fontSize: 26),
+  AppBar _appBar() {
+    return AppBar(
+      backgroundColor: const Color.fromARGB(255, 13, 10, 51),
+      title: const Text(
+        'Tapshyrma 7',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
         ),
       ),
+      centerTitle: true,
     );
   }
 }
